99fe618bc5e519a3ee0ed0d00cd6119fa7938464
Stas <stasn139@gmail.com>
2024-11-14 12:41:18 +0000

    add mock data

--- frontend/src/components/Statistics.tsx
+++ frontend/src/components/Statistics.tsx
@@ -1,5 +1,34 @@
 import { useReadContract } from 'wagmi';
 import { Card } from '../ui/Card';
+import BadgeCard from '../ui/BadgeCard';
+
+import badge1 from '../assets/64182.svg';
+import badge2 from '../assets/64183.svg';
+import badge3 from '../assets/66917.svg';
+import badge4 from '../assets/66918.svg';
+
+const badgesData = [
+    {
+        name: "Badge",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, repudiandae iste placeat perferendis inventore, quibusdam dolor, possimus nemo quia odio illo a necessitatibus neque tempore culpa aspernatur veritatis provident? Possimus. Nobis, suscipit? Commodi assumenda voluptate saepe asperiores illum eos iste tempora accusantium corrupti minima. Blanditiis quia similique, ab quibusdam deserunt nostrum labore. Magnam animi quos iure qui aliquid adipisci quaerat?",
+        img: badge1
+    },
+    {
+        name: "Badge",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, repudiandae iste placeat perferendis inventore, quibusdam dolor, possimus nemo quia odio illo a necessitatibus neque tempore culpa aspernatur veritatis provident? Possimus. Nobis, suscipit? Commodi assumenda voluptate saepe asperiores illum eos iste tempora accusantium corrupti minima. Blanditiis quia similique, ab quibusdam deserunt nostrum labore. Magnam animi quos iure qui aliquid adipisci quaerat?",
+        img: badge2
+    },
+    {
+        name: "Badge",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, repudiandae iste placeat perferendis inventore, quibusdam dolor, possimus nemo quia odio illo a necessitatibus neque tempore culpa aspernatur veritatis provident? Possimus. Nobis, suscipit? Commodi assumenda voluptate saepe asperiores illum eos iste tempora accusantium corrupti minima. Blanditiis quia similique, ab quibusdam deserunt nostrum labore. Magnam animi quos iure qui aliquid adipisci quaerat?",
+        img: badge3
+    },
+    {
+        name: "Badge",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, repudiandae iste placeat perferendis inventore, quibusdam dolor, possimus nemo quia odio illo a necessitatibus neque tempore culpa aspernatur veritatis provident? Possimus. Nobis, suscipit? Commodi assumenda voluptate saepe asperiores illum eos iste tempora accusantium corrupti minima. Blanditiis quia similique, ab quibusdam deserunt nostrum labore. Magnam animi quos iure qui aliquid adipisci quaerat?",
+        img: badge4
+    }
+]
 
 const Statistics = () => {
 
@@ -14,6 +43,12 @@ const Statistics = () => {
     return (
         <Card className="bg-foreground-light">
             <section className="mt-8">Historical data of user lend/borrow interaction with calculating of Health Rate</section>
+            <div className="flex gap-3">
+                {badgesData.map(badge => <BadgeCard badge={badge}/>)}
+                {/* <BadgeCard />
+                <BadgeCard />
+                <BadgeCard /> */}
+            </div>
         </Card>
     )
 }
