1472dbb5b889d8b57d08f4f41528a223a291dc14
Stas <stasn139@gmail.com>
2024-11-16 20:45:15 +0000

    refactor provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,23 @@ import Statistics from './components/Statistics'
 import { Button } from './ui/Button'
 import { Card } from './ui/Card'
 import { wagmiConfig as config, Web3Provider } from './web3provider'
+import Badges from './components/Badges'
 
 function App() {
 
   return (
     <Web3Provider>
-      <div className="flex flex-row gap-4 flex-wrap">
-        <Statistics />
-        <Card className="bg-foreground-light w-[320px]">statistics list (health score, available value, percentage etc)</Card>
-        <Card className="bg-foreground-light flex-1">commercial with CTA to check eligibility another wallet</Card>
-        <Card className="bg-foreground-light" wrapperClassName="w-full">badges(attestattions) list</Card>
-        <section className="mt-8 w-full">APY of various tokens through some lend/borrow protocols (aave, ...)</section>
-        <Button variant="primary">tst</Button>
+      <div className="flex gap-8">
+        <div className="bg-foreground-light">menu</div>
+        <div className="flex flex-row gap-4 flex-wrap">
+          <Badges />
+          <Statistics className="w-[320px]"/>
+          <Card className="bg-foreground-light flex-1">commercial with CTA to check eligibility another wallet <Button variant="primary">tst</Button></Card>
+          <Card className="bg-foreground-light" wrapperClassName="w-full">badges(attestattions) list</Card>
+          <section className="mt-8 w-full">APY of various tokens through some lend/borrow protocols (aave, ...)</section>
+        </div>
       </div>
+      powered by Verax (2024)
     </Web3Provider>
   )
 }
--- frontend/src/web3provider.tsx
+++ frontend/src/web3provider.tsx
@@ -1,18 +1,16 @@
-import { lineaSepolia, mainnet } from 'wagmi/chains';
+import { lineaSepolia, mainnet, sepolia} from 'wagmi/chains';
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createConfig, WagmiProvider } from 'wagmi';
 
 export const walletConnectProjectId = 'd149fd0b3fe44636121032492ed688e2';
-const chains = [lineaSepolia, mainnet] as const;
+const chains = [sepolia, lineaSepolia, mainnet] as const;
 export const wagmiConfig = createConfig(
   getDefaultConfig({
     chains,
     walletConnectProjectId,
-    appName: 'eFrogs Attestations',
-    appDescription: 'Issue attestation of eFrogs ownership',
-    appUrl: 'https://efrogs.alainnicolas.fr',
-    appIcon: 'https://efrogs.alainnicolas.fr/favicon.jpg',
+    appName: 'BankWatch',
+    appDescription: 'Issue attestation to proof that you are reliable borrower',
   }),
 );
 
