c81c872adca93978d4995a64778bf33a0b771cfe
Stas <stasn139@gmail.com>
2024-11-22 00:01:21 +0000

    update colors

--- frontend/src/ui/Button/index.tsx
+++ frontend/src/ui/Button/index.tsx
@@ -2,16 +2,19 @@ import { forwardRef } from 'react';
 import clsx from "clsx";
 
 import { ButtonProps, buttonVariants } from './types';
+import { WheelIcon } from '../../assets/icons/Wheel';
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, variant, size, isLoading, ...props }, ref) => {
     return (
       <button
         type="button"
         className={clsx(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {isLoading ? <WheelIcon className="animate-spin-loader" /> : props.children}
+      </button>
     );
   },
 );
