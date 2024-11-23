7a33b272c5b0c0b635ba8a7956e13d155d91ca75
Stas <stasn139@gmail.com>
2024-11-23 23:23:39 +0000

    imrove button loading

--- frontend/src/components/Badges.tsx
+++ frontend/src/components/Badges.tsx
@@ -6,18 +6,20 @@ import { useEffect, useState } from 'react';
 import { Attestation, VeraxSdk } from '@verax-attestation-registry/verax-sdk';
 import { Typography } from '../ui/Typography';
 import clsx from 'clsx';
+import Toast from '../ui/Toast';
 
 const Badges = ({ className, veraxSdk }: { className?: string, veraxSdk: VeraxSdk }) => {
     // const [address, setAddress] = useState("0x230cDe8909aeBBc48CfBDf6fCc9A642439d77F83")
     const { address, chainId, isConnected, chain } = useAccount();
     const [attestations, setAttestations] = useState<Attestation[]>([]);
-    const [issueLoading, setIssueLoading] = useState(false);
+    const [issueLoading, setIssueLoading] = useState<string | boolean>(false);
     const [revealLoading, setRevealLoading] = useState(false);
-    
 
-    const issueAttestation = async (schemaId: string) => {
+
+    const issueAttestation = async (schemaId: string, badgeName: string) => {
         if (!address) return;
-        setIssueLoading(true);
+        setIssueLoading(badgeName);
+        console.log({ badge_claimed: true, [badgeName]: address })
         try {
             let receipt = await veraxSdk.portal.attest(
                 LINEA_SEPOLIA_PORTAL_ADDRESS,
@@ -25,8 +27,7 @@ const Badges = ({ className, veraxSdk }: { className?: string, veraxSdk: VeraxSd
                     schemaId,
                     expirationDate: 1756741729,
                     subject: address,
-                    // claimed_by: address
-                    attestationData: [{ badge_claimed: true }], // todo: check args
+                    attestationData: [{ badge_claimed: true, [badgeName]: address }],
                 },
                 [],
             );
@@ -50,7 +51,7 @@ const Badges = ({ className, veraxSdk }: { className?: string, veraxSdk: VeraxSd
     const revealAttestations = async () => {
         setRevealLoading(true);
         try {
-            const attestationsList = await veraxSdk.attestation.findBy(1, 0, { portal: LINEA_SEPOLIA_PORTAL_ADDRESS, subject: address });
+            const attestationsList = await veraxSdk.attestation.findBy(500, 0, { portal: LINEA_SEPOLIA_PORTAL_ADDRESS, subject: address });
             setAttestations(attestationsList)
         } catch (e) {
             console.log(`${e}`);
@@ -76,16 +77,19 @@ const Badges = ({ className, veraxSdk }: { className?: string, veraxSdk: VeraxSd
             <div className="flex flex-wrap gap-3">
                 {badgesData.map(badge => {
                     const attested = !!attestations.filter(attestation => attestation.schema.id === badge.schema).length;
+                    console.log(attestations);
                     return (
                         <BadgeCard
                             badge={badge}
                             key={badge.name}
-                            onClick={() => issueAttestation(badge.schema)}
+                            onClick={() => issueAttestation(badge.schema, badge.badgeName)}
                             attested={attested}
-                            loading={revealLoading || issueLoading}
+                            loading={revealLoading || (issueLoading === badge.badgeName)}
                         />)
                 })}
             </div>
+            {/* TODO: implement context for Toasts */}
+            {/* <Toast success/>  */}
         </Card>
     )
 }
