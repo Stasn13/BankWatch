880dfbcc98316d27adb746f078dba99273f36217
Stas <stasn139@gmail.com>
2024-11-23 00:40:15 +0000

    update naviagation layout

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import { Card } from "../ui/Card"
 
 const Navigation = () => {
     return (
-        <Card className="bg-foreground-light h-[auto] flex flex-col gap-4 pt-4 px-2 pb-6 mt-4 items-center">
+        <Card className="bg-foreground-light h-[fit-content] flex flex-col gap-4 pt-4 px-2 pb-6 mt-4 items-center">
             <div className="w-[48px] h-[48px] rounded-lg bg-black grid justify-center items-center font-bold">BW</div>
             <DashboardIcon className="border rounded-lg p-2"/>
             <SearchIcon className="rounded-lg p-2"/> 
