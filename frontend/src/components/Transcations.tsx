34fb5f833ad48e8acc71400c90346c5f17f4e9f1
Stas <stasn139@gmail.com>
2024-11-26 15:57:12 +0000

    add coming soon label

--- frontend/src/components/Transcations.tsx
+++ frontend/src/components/Transcations.tsx
@@ -20,9 +20,16 @@ const Transactions = ({ className, wrapperClassName, bgImg, text, btnText, child
     return (
         <Card
             className={clsx(className, "bg-foreground-light text-left")}
-            wrapperClassName={clsx(wrapperClassName, "overflow-hidden")}
+            wrapperClassName={clsx(wrapperClassName, "overflow-hidden w-full relative")}
         >
-            transactions coming soon...
+            <Typography
+                className="mb-4"
+                size='heading5'
+            >
+                Transactions list
+            </Typography>
+
+            <Typography className="rounded-xl border px-2 absolute top-3 right-3 font-black">Coming soon ...</Typography>
             {/* {
   repays(where:{user: "0x230cDe8909aeBBc48CfBDf6fCc9A642439d77F83"}, orderBy: blockTimestamp, orderDirection: desc) {
     user
